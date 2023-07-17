416e3e9bc3237e5dfca3a8354fdf3d0d6a080280
Ben Krepp <bkrepp@ctps.org>
2023-07-17 16:26:23 -0400

    Basic plumbing to viz-generator now in place - tweaks will be required

--- js/viz_example_1.js
+++ js/viz_example_1.js
@@ -12,20 +12,67 @@ function bar_chart(div_id, data) {
 	Plotly.newPlot(div_id, data);
 }
 
-function generate_classified_viz(count) {
+function generate_classified_viz(count1, count2) {
+	
+	// WARNING: HACK HERE - we know that the 2 counts differ in Date
+	var date1 = count1[0].count_date.substr(0,10);
+	var date2 = count2[0].count_date.substr(0,10);
+	
 	var trace1 = {
-	  x: ['giraffes', 'orangutans', 'monkeys'],
-	  y: [20, 14, 23],
-	  name: 'SF Zoo',
+	  x: ['Bicycle', 'Jogger', 'Skater', 'Wheelchair', 'Other', 'Baby Carriage'],
+	  y: [0, 0 , 0, 0, 0, 0],
+	  name: date1,
 	  type: 'bar'
 	};
-
+	
+	// Populate the array of 'y' values for count1
+	var temp;
+	temp = _.find(count1, function(rec) { return rec.count_type == 'B' });
+	trace1.y[0] = temp.cnt_total;
+	
+	temp = _.find(count1, function(rec) { return rec.count_type == 'J' });
+	trace1.y[1] = temp.cnt_total;
+	
+	temp = _.find(count1, function(rec) { return rec.count_type == 'S' });
+	trace1.y[2] = temp.cnt_total;
+	
+	temp = _.find(count1, function(rec) { return rec.count_type == 'W' });
+	trace1.y[3] = temp.cnt_total;
+	
+	temp = _.find(count1, function(rec) { return rec.count_type == 'O' });
+	trace1.y[4] = temp.cnt_total;
+	
+	// *** WARNING - HACK HERE - WE KNOW THERE IS NO 'C' RECORD AS PART OF THE 1ST COUNT
+	temp = _.find(count1, function(rec) { return rec.count_type == 'C' });
+	trace1.y[5] = 0
+	
+	
 	var trace2 = {
-	  x: ['giraffes', 'orangutans', 'monkeys'],
-	  y: [12, 18, 29],
-	  name: 'LA Zoo',
+	  x: ['Jogger', 'Skater', 'Wheelchair', 'Other', 'Baby Carriage'],
+	  y: [0, 0 , 0, 0, 0, 0],
+	  name: date2,
 	  type: 'bar'
 	};
+	
+	temp = _.find(count2, function(rec) { return rec.count_type == 'B' });
+	trace2.y[0] = temp.cnt_total;
+	
+	temp = _.find(count2, function(rec) { return rec.count_type == 'J' });
+	trace2.y[1] = temp.cnt_total;
+	
+	temp = _.find(count2, function(rec) { return rec.count_type == 'S' });
+	trace2.y[2] = temp.cnt_total;
+	
+	temp = _.find(count2, function(rec) { return rec.count_type == 'W' });
+	trace2.y[3] = temp.cnt_total;
+	
+	// *** WARNING - HACK HERE - WE KNOW THERE IS NO 'O' RECORD AS PART OF THE 2ND COUNT
+	temp = _.find(count2, function(rec) { return rec.count_type ==' O' });
+	trace2.y[4] = 0;
+	
+	temp = _.find(count2, function(rec) { return rec.count_type == 'C' });
+	trace2.y[5] = temp.cnt_total;
+
 
 	var data = [trace1, trace2];
 
@@ -43,11 +90,11 @@ function generate_viz() {
 			var uniq_counts = _.uniqBy(count_data, 'count_id');
 			var uniq_count_ids = _.map(uniq_counts, 'count_id')
 			_DEBUG_HOOK = 1;
-			console.log('Number of unique counts = ' + uniq_count_ids.length);
-			// Just process the 1st one to establish proof-of-concept, for now
-			var count_id = uniq_count_ids[0];
-			var count_to_process = _.filter(count_data, function(rec) { return rec.count_id == count_id; } );
-			generate_classified_viz(count_to_process);
+			console.log('Number of unique counts = ' + uniq_count_ids.length); // should be 2
+			
+			var count_1 = _.filter(count_data, function(rec) { return rec.count_id == uniq_count_ids[0]; } );
+			var count_2 = _.filter(count_data, function(rec) { return rec.count_id == uniq_count_ids[1]; } );
+			generate_classified_viz(count_1, count_2);
 		});
 
 	_DEBUG_HOOK = 2;
