938596d86616a721faa4d67f91c17cd50bfdb017
Benjamin Krepp <bkrepp@ctps.org>
2023-10-10 10:07:45 -0400

    Moved download logic into common.js

--- js/common.js
+++ js/common.js
@@ -505,3 +505,98 @@ function popup(url) {
 displayMetadata = function() {
 	popup('metadata.html');
 }; // displayMetadata
+
+// Format the array of data in 'counts' as CSV and download it
+function download_data(counts) {
+	var s; // string into which data to be downloaded in CSV format will be accumulated
+	var header = "id,bp_loc_id,count_id,town,";
+		header += "description,temperature,sky,count_type,";
+		header += "from_st_name,from_st_dir,to_st_name,to_st_dir,count_date,count_dow,";
+		header += "cnt_0630,cnt_0645,";
+		header += "cnt_0700,cnt_0715,cnt_0730,cnt_0745,cnt_0800,cnt_0815,cnt_0830,cnt_0845,cnt_0900,cnt_0915,cnt_0930,cnt_0945,";
+		header += "cnt_1000,cnt_1015,cnt_1030,cnt_1045,cnt_1100,cnt_1115,cnt_1130,cnt_1145,cnt_1200,cnt_1215,cnt_1230,cnt_1245,";
+		header += "cnt_1300,cnt_1315,cnt_1330,cnt_1345,cnt_1400,cnt_1415,cnt_1430,cnt_1445,cnt_1500,cnt_1515,cnt_1530,cnt_1545,";
+		header += "cnt_1600,cnt_1615,cnt_1630,cnt_1645,cnt_1700,cnt_1715,cnt_1730,cnt_1745,cnt_1800,cnt_1815,cnt_1830,cnt_1845,";
+		header += "cnt_1900,cnt_1915,cnt_1930,cnt_1945,cnt_2000,cnt_2015,cnt_2030,cnt_2045,cnt_total";
+		header += "\n";
+		
+		s = header;
+		counts.forEach(function(c) {
+            s += c.id + ',';
+            s += c.bp_loc_id + ',';
+            s += c.count_id + ',';
+            s += c.town + ',';
+            s += '"' + c.description + '",';
+            s += c.temperature + ',';
+            s += c.sky + ',';
+            s += c.count_type + ',';
+            s += c.from_st_name + ',';
+            s += c.from_st_dir + ',';
+            s += c.to_st_name + ',';
+            s += c.to_st_dir + ',';
+            s += '"' + c.count_date + '",';
+            s += c.count_dow + ',';
+            s += c.cnt_0630 + ',';
+            s += c.cnt_0645 + ',';
+            s += c.cnt_0700 + ',';
+            s += c.cnt_0715 + ',';
+            s += c.cnt_0730 + ',';
+            s += c.cnt_0745 + ',';
+            s += c.cnt_0800 + ',';
+            s += c.cnt_0815 + ',';
+            s += c.cnt_0830 + ',';
+            s += c.cnt_0845 + ',';
+            s += c.cnt_0900 + ',';
+            s += c.cnt_0915 + ',';
+            s += c.cnt_0930 + ',';
+            s += c.cnt_0945 + ',';
+            s += c.cnt_1000 + ',';
+            s += c.cnt_1015 + ',';
+            s += c.cnt_1030 + ',';
+            s += c.cnt_1045 + ',';
+            s += c.cnt_1100 + ',';
+            s += c.cnt_1115 + ',';
+            s += c.cnt_1130 + ',';
+            s += c.cnt_1145 + ',';
+            s += c.cnt_1200 + ',';
+            s += c.cnt_1215 + ',';
+            s += c.cnt_1230 + ',';
+            s += c.cnt_1245 + ',';
+            s += c.cnt_1300 + ',';
+            s += c.cnt_1315 + ',';
+            s += c.cnt_1330 + ',';
+            s += c.cnt_1345 + ',';
+            s += c.cnt_1400 + ',';
+            s += c.cnt_1415 + ',';
+            s += c.cnt_1430 + ',';
+            s += c.cnt_1445 + ',';
+            s += c.cnt_1500 + ',';
+            s += c.cnt_1515 + ',';
+            s += c.cnt_1530 + ',';
+            s += c.cnt_1545 + ',';
+            s += c.cnt_1600 + ',';
+            s += c.cnt_1615 + ',';
+            s += c.cnt_1630 + ',';
+            s += c.cnt_1645 + ',';
+            s += c.cnt_1700 + ',';
+            s += c.cnt_1715 + ',';
+            s += c.cnt_1730 + ',';
+            s += c.cnt_1745 + ',';
+            s += c.cnt_1800 + ',';
+            s += c.cnt_1815 + ',';
+            s += c.cnt_1830 + ',';
+            s += c.cnt_1845 + ',';
+            s += c.cnt_1900 + ',';
+            s += c.cnt_1915 + ',';
+            s += c.cnt_1930 + ',';
+            s += c.cnt_1945 + ',';
+            s += c.cnt_2000 + ',';
+            s += c.cnt_2015 + ',';
+            s += c.cnt_2030 + ',';
+            s += c.cnt_2045 + ',';
+            s += c.cnt_total + ',';
+			s += "\n";
+		});
+		// HERE: 's' is the entire string to download
+		download(s, 'bike_ped_counts', 'text/csv');
+} // download_data
